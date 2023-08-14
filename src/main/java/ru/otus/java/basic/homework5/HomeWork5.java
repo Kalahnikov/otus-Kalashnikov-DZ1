223953ed76c4a170d9680a0c2bed287b9b210c21
Your Name <you@example.com>
2023-08-14 16:56:57 +0300

    дз 5 выполнено

--- src/main/java/ru/otus/java/basic/homework5/HomeWork5.java
+++ src/main/java/ru/otus/java/basic/homework5/HomeWork5.java
@@ -1,15 +1,19 @@
 package ru.otus.java.basic.homework5;
 
+import ru.otus.java.basic.homework5.animals.Cat;
+import ru.otus.java.basic.homework5.animals.Dog;
+import ru.otus.java.basic.homework5.animals.Horse;
+
 public class HomeWork5 {
     public static void main(String[] args) {
         Cat cat = new Cat("Барсик", 7, 25);
-        Hourse hourse = new Hourse("Марс", 15, 2, 60);
+        Horse horse = new Horse("Марс", 15, 2, 60);
         Dog dog = new Dog("Пес", 10, 3, 40);
         cat.run(75);
         cat.swim(12);
         dog.run(20);
         dog.swim(15);
-        hourse.run(55);
-        hourse.swim(16);
+        horse.run(55);
+        horse.swim(16);
     }
 }
--- src/main/java/ru/otus/java/basic/homework5/animals/Cat.java
+++ src/main/java/ru/otus/java/basic/homework5/animals/Cat.java
@@ -1,15 +1,18 @@
-package ru.otus.java.basic.homework5;
+package ru.otus.java.basic.homework5.animals;
 
-public class Cat extends Animal{
+import ru.otus.java.basic.homework5.animals.Animal;
+
+public class Cat extends Animal {
     public Cat(String name, int speedRunning, int stamina) {
         this.name = name;
         this.speedRunning = speedRunning;
         this.stamina = stamina;
+        super.type = "Кот";
     }
 
     @Override
     public int swim(int distance) {
         System.out.println("Коты не умеют плавать, пусть отдыхают");
-        return 0;
+        return -1;
     }
 }
